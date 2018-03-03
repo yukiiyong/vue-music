b578a875d2b74c4a838a33e31cb230daddc666c8
yukiiyong <911711448@qq.com>
2018-03-04 01:35:23 +0800

    build

--- build/build.js
+++ build/build.js
@@ -1,4 +1,3 @@
-'use strict'
 require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
@@ -27,10 +26,10 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       chunkModules: false
     }) + '\n\n')
 
-    if (stats.hasErrors()) {
+   /* if (stats.hasErrors()) {
       console.log(chalk.red('  Build failed with errors.\n'))
       process.exit(1)
-    }
+    }*/
 
     console.log(chalk.cyan('  Build complete.\n'))
     console.log(chalk.yellow(
--- prod.server.js
+++ prod.server.js
@@ -1,27 +1,27 @@
 /*
 * @Author: yuki
 * @Date:   2018-01-02 16:37:15var express=
-* @Last Modified by:   yuki
-* @Last Modified time: 2018-01-02 17:21:20
+* @Last Modified by:   yukiiyong
+* @Last Modified time: 2018-03-04 01:34:00
 */
-var express=require('express')
-var config=require('./config/index')
-var axios =require('axios')
+var express = require('express')
+var config = require('./config/index')
+var axios = require('axios')
 
-var port=process.env.PORT ||config.build.port
+var port = process.env.PORT ||config.build.port
 
-var qpp= express()
+var app = express()
 
-var qpiRoutes=express.Router()
+var apiRoutes = express.Router()
 
 apiRoutes.get('/getDiscList',function(req,res){
-	var url= 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+	var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
 	axios.get(url,{
 		headers:{
 			referer:'https://c.y.qq.com/',
 			host:'c.y.qq.com'
 		},
-		params:requ.query
+		params:req.query
 	}).then((response) => {
 		res.json((response.data))
 	}).catch((e) => {
@@ -54,7 +54,29 @@ apiRoutes.get('/lyric',function(req,res){
 	})
 })
 
-app.use('/api',apiRputes)
+apiRoutes.get('/songlist', function(req, res) {
+  const url='https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+  axios.get(url,{
+    headers: {
+      referer: 'https://c.y.qq.com/'
+    },
+    params: req.query
+  }).then((response) => {
+    var ret = response.data
+    if(typeof ret === 'string') {
+      var reg = /^\w+\(({[^()]})\)$/
+      var matches = ret.match(reg)
+      if(matches) {
+        ret = JSON.parse(matches[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+app.use('/api',apiRoutes)
 
 app.use(express.static('./dist'))
 
